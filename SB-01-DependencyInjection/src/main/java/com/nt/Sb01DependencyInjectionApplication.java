46c96cccd14ef9efbeb2766a8d263907dcf1f036
Subodh <welcome@DESKTOP-4BFJCBM>
2021-06-12 18:18:14 +0530

    ConfigurationCumClient Application is modified.

--- SB-01-DependencyInjection/src/main/java/com/nt/Sb01DependencyInjectionApplication.java
+++ SB-01-DependencyInjection/src/main/java/com/nt/Sb01DependencyInjectionApplication.java
@@ -1,6 +1,7 @@
 package com.nt;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 
 import com.nt.beans.WishMessageGenerator;
 
@@ -26,6 +28,7 @@ public class Sb01DependencyInjectionApplication {
 	}// createLocalDateTime()
 
 	@Bean(name = "ldt1")
+	@Scope(value = "prototype")
 	public LocalDateTime createLocalDateTime1() {
 		System.out.println("Sb01DependencyInjectionApplication.createLocalDateTime1()");
 		return LocalDateTime.now();
@@ -34,16 +37,32 @@ public class Sb01DependencyInjectionApplication {
 	public static void main(String[] args) {
 		System.out.println("Sb01DependencyInjectionApplication.main(-)");
 		ApplicationContext ctx = null;
-		WishMessageGenerator wmg = null;
+		WishMessageGenerator wmg1 = null;
+		WishMessageGenerator wmg2 = null;
 		String result = null;
 		// access IOC container
 		ctx = SpringApplication.run(Sb01DependencyInjectionApplication.class, args);
-		// get target class object
-		wmg = ctx.getBean("wmg", WishMessageGenerator.class);
+		// get ConfigurationCumClientApplication bean class name
+		Sb01DependencyInjectionApplication bean1 = ctx.getBean("sb01DependencyInjectionApplication",
+				Sb01DependencyInjectionApplication.class);
+		System.out.println(bean1.getClass() + " <-> " + bean1.hashCode());
+		System.out.println("-------------------------------------------------");
+		// get LocalDateTime bean class name
+		LocalDateTime bean2 = ctx.getBean("ldt1", LocalDateTime.class);
+		System.out.println(bean2.getClass() + " <-> " + bean2.hashCode());
+		System.out.println("-------------------------------------------------");
+		System.out.println("All Beans' IDs : " + Arrays.toString(ctx.getBeanDefinitionNames()));
+		System.out.println("Beans Count : " + ctx.getBeanDefinitionCount());
+		System.out.println("-------------------------------------------------");
+		// get target class object from IOC container
+		wmg1 = ctx.getBean("wmg", WishMessageGenerator.class);
 		// invoke business method
-		result = wmg.generateWishMsg("Sri Rama");
+		result = wmg1.generateWishMsg("Sri Rama");
 		// display the result
 		System.out.println(result);
+		wmg2 = ctx.getBean("wmg", WishMessageGenerator.class);
+		System.out.println(wmg1.hashCode() + " <-> " + wmg2.hashCode());
+		System.out.println("Is wmg1 == wmg2 ? " + (wmg1 == wmg2));
 		// close IOC container
 		((ConfigurableApplicationContext) ctx).close();
 	}// main(-)
