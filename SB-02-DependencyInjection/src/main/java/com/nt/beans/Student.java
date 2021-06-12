afe52b23f0affa6d5c75ed024fef96944cde9c89
Subodh <welcome@DESKTOP-4BFJCBM>
2021-06-13 00:56:06 +0530

    Made target bean as final class.

--- SB-02-DependencyInjection/src/main/java/com/nt/beans/Student.java
+++ SB-02-DependencyInjection/src/main/java/com/nt/beans/Student.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("std")
-public class Student {
+public final class Student {
 
 	@Autowired()
 	// @Qualifier("php")
